b6cc3bdc8847b162970245267b194f1227ff9e05
Josh Knauer <jknauer@mac.lan>
2025-08-08 14:01:40 -0700

    Fix: Switch to River Watch Live interface and add missing water level CSS classes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,10 @@
-import { MapInterface } from "@/components/MapInterface";
+import { RiverGaugeMap } from "@/components/RiverGaugeMap";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <div className="container mx-auto p-4">
-        <MapInterface />
+        <RiverGaugeMap apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY || ''} />
       </div>
     </div>
   );
